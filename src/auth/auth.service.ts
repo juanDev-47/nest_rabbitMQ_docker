090d633cd92da9b92c1ab9d948edb7cb9c50ace9
juanDev-47 <juanp.arenas@udea.edu.co>
2022-09-27 22:07:14 -0500

    new: implement authentication and authoritation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,10 +6,12 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) {}
 
   async signup(dto: AuthDto) {
     // hash the password
@@ -67,9 +69,23 @@ export class AuthService {
           'Invalid credentials',
         );
       }
-      // if password is correct return the user
-      delete user.hash;
-      return user;
+      // if password is correct return the Token
+      return this.signToken(user.id, user.email);
     }
   }
+
+  async signToken(userId: number, email: string): Promise<{access_token: string}> {
+    const payload = {
+      userId,
+      email,
+    };
+    const token = await this.jwt.signAsync(payload, {
+      secret: this.config.get('JWT_SECRET'),
+      expiresIn: '1d',
+    });
+    return {
+      access_token: token,
+    }
+  }
+
 }
