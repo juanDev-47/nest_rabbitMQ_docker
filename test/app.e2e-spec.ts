8f94e0f66fe4d4112f5256cee9383f62f0dfb60f
juanDev-47 <juanp.arenas@udea.edu.co>
2022-10-10 14:33:50 -0500

    updated: modified test files

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,29 @@
+import {
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 
 describe('App E2E', () => {
+  let app: INestApplication;
   beforeAll(async () => {
     const moduleRef =
       await Test.createTestingModule({
         imports: [AppModule],
       }).compile();
+
+    const app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
   it.todo('should pass');
 });
